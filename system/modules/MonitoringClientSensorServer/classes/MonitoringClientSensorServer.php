a68f3018d389518ebb2961a137ae3b918cf35605
Cliff Parnitzky <github@cliff-parnitzky.de>
2017-11-20 23:08:56 +0100

    Adding Contao 4 compatibility

--- system/modules/MonitoringClientSensorServer/classes/MonitoringClientSensorServer.php
+++ system/modules/MonitoringClientSensorServer/classes/MonitoringClientSensorServer.php
@@ -55,23 +55,64 @@ class MonitoringClientSensorServer extends \Backend
    */
   public function readData($arrData)
   {
+    $arrPhpInfo = $this->phpinfo_array();
     // PHP
     $arrIniGetAll = ini_get_all();
-    $arrData['php.max_execution_time'] = $arrIniGetAll['max_execution_time']['local_value'];
-    $arrData['php.memory_limit'] = $arrIniGetAll['memory_limit']['local_value'];
-    $arrData['php.version'] = phpversion();
+    $arrData['php.max_execution_time'] = $arrPhpInfo['Core']['max_execution_time'];
+    $arrData['php.memory_limit'] = $arrPhpInfo['Core']['memory_limit'];
+    $arrData['php.version'] = $arrPhpInfo['Core']['PHP Version'];
     // Server
     $arrData['server.os'] = php_uname();
-    $arrData['server.software'] = $_SERVER['SERVER_SOFTWARE'];
-    // MySQL (need hacky reflection here)
-    $objDatabase = \Database::getInstance();
-    $resConnection = new \ReflectionProperty($objDatabase,'resConnection');
-    $resConnection->setAccessible(true);
-    $arrData['mysql.version'] = mysqli_get_server_info($resConnection->getValue($objDatabase));
-    $resConnection->setAccessible(false);
+    $arrData['server.software'] = $arrPhpInfo['Environment']['SERVER_SOFTWARE'];
+    // MySQL
+    $version = \Database::getInstance()->prepare("SELECT @@version as version")->execute()->version;
+    $arrData['mysql.version'] = substr($version, 0, strpos($version, "-"));
     
     return $arrData;
   }
+  
+  /**
+   * Put phpinfo data in a parsable array (see http://www.php.net/manual/en/function.phpinfo.php#87463)
+   */
+  function phpinfo_array()
+  {
+    ob_start(); 
+    phpinfo(-1);
+
+    $pi = preg_replace(
+    array('#^.*<body>(.*)</body>.*$#ms', '#<h2>PHP License</h2>.*$#ms',
+    '#<h1>Configuration</h1>#',  "#\r?\n#", "#</(h1|h2|h3|tr)>#", '# +<#',
+    "#[ \t]+#", '#&nbsp;#', '#  +#', '# class=".*?"#', '%&#039;%',
+      '#<tr>(?:.*?)" src="(?:.*?)=(.*?)" alt="PHP Logo" /></a>'
+      .'<h1>PHP Version (.*?)</h1>(?:\n+?)</td></tr>#',
+      '#<h1><a href="(?:.*?)\?=(.*?)">PHP Credits</a></h1>#',
+      '#<tr>(?:.*?)" src="(?:.*?)=(.*?)"(?:.*?)Zend Engine (.*?),(?:.*?)</tr>#',
+      "# +#", '#<tr>#', '#</tr>#'),
+    array('$1', '', '', '', '</$1>' . "\n", '<', ' ', ' ', ' ', '', ' ',
+      '<h2>PHP Configuration</h2>'."\n".'<tr><td>PHP Version</td><td>$2</td></tr>'.
+      "\n".'<tr><td>PHP Egg</td><td>$1</td></tr>',
+      '<tr><td>PHP Credits Egg</td><td>$1</td></tr>',
+      '<tr><td>Zend Engine</td><td>$2</td></tr>' . "\n" .
+      '<tr><td>Zend Egg</td><td>$1</td></tr>', ' ', '%S%', '%E%'),
+    ob_get_clean());
+
+    $sections = explode('<h2>', strip_tags($pi, '<h2><th><td>'));
+    unset($sections[0]);
+
+    $pi = array();
+    foreach($sections as $section)
+    {
+       $n = substr($section, 0, strpos($section, '</h2>'));
+       preg_match_all('#%S%(?:<td>(.*?)</td>)?(?:<td>(.*?)</td>)?(?:<td>(.*?)</td>)?%E%#', $section, $askapache, PREG_SET_ORDER);
+       foreach($askapache as $m)
+       {
+         $pi[$n][$m[1]]=(!isset($m[3])||$m[2]==$m[3])?$m[2]:array_slice($m,2);
+       }
+    }
+
+    return $pi;
+  }
+
 }
 
 ?>
