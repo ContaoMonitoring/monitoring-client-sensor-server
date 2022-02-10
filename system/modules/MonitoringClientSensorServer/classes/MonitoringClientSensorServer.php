6024a150352fa35edb5ee170b8ba3d01fed8b13d
Cliff Parnitzky <github@cliff-parnitzky.de>
2022-02-11 00:34:33 +0100

    Improves method to get the PHP variables to fix problems with PHP 8 (closes #2)

--- system/modules/MonitoringClientSensorServer/classes/MonitoringClientSensorServer.php
+++ system/modules/MonitoringClientSensorServer/classes/MonitoringClientSensorServer.php
@@ -2,7 +2,7 @@
 
 /**
  * Contao Open Source CMS
- * Copyright (C) 2005-2017 Leo Feyer
+ * Copyright (C) 2005-2022 Leo Feyer
  *
  * Formerly known as TYPOlight Open Source CMS.
  *
@@ -21,7 +21,7 @@
  * Software Foundation website at <http://www.gnu.org/licenses/>.
  *
  * PHP version 5
- * @copyright  Cliff Parnitzky 2017-2017
+ * @copyright  Cliff Parnitzky 2017-2022
  * @author     Cliff Parnitzky
  * @package    MonitoringClientSensorServer
  * @license    LGPL
@@ -36,7 +36,7 @@ namespace Monitoring;
  * Class MonitoringClientSensorServer
  *
  * Special sensor for the MonitoringClient to read the server data.
- * @copyright  Cliff Parnitzky 2017-2017
+ * @copyright  Cliff Parnitzky 2017-2022
  * @author     Cliff Parnitzky
  * @package    Controller
  */
@@ -55,64 +55,19 @@ class MonitoringClientSensorServer extends \Backend
    */
   public function readData($arrData)
   {
-    $arrPhpInfo = $this->phpinfo_array();
     // PHP
-    $arrIniGetAll = ini_get_all();
-    $arrData['php.max_execution_time'] = $arrPhpInfo['Core']['max_execution_time'];
-    $arrData['php.memory_limit'] = $arrPhpInfo['Core']['memory_limit'];
-    $arrData['php.version'] = $arrPhpInfo['Core']['PHP Version'];
+    $arrData['php.max_execution_time'] = ini_get('max_execution_time');
+    $arrData['php.memory_limit'] = ini_get('memory_limit');
+    $arrData['php.version'] = phpversion();
     // Server
     $arrData['server.os'] = php_uname();
-    $arrData['server.software'] = $arrPhpInfo['PHP Variables'][current(preg_grep("/(.)*_SERVER\[.SERVER_SOFTWARE.\]/", array_keys($arrPhpInfo['PHP Variables'])))]; 
+    $arrData['server.software'] = $_SERVER['SERVER_SOFTWARE']; 
     // MySQL
     $version = \Database::getInstance()->prepare("SELECT @@version as version")->execute()->version;
     $arrData['mysql.version'] = strpos($version, "-") ? substr($version, 0, strpos($version, "-")) : $version;
     
     return $arrData;
   }
-  
-  /**
-   * Put phpinfo data in a parsable array (see http://www.php.net/manual/en/function.phpinfo.php#87463)
-   */
-  function phpinfo_array()
-  {
-    ob_start(); 
-    phpinfo(-1);
-
-    $pi = preg_replace(
-    array('#^.*<body>(.*)</body>.*$#ms', '#<h2>PHP License</h2>.*$#ms',
-    '#<h1>Configuration</h1>#',  "#\r?\n#", "#</(h1|h2|h3|tr)>#", '# +<#',
-    "#[ \t]+#", '#&nbsp;#', '#  +#', '# class=".*?"#', '%&#039;%',
-      '#<tr>(?:.*?)" src="(?:.*?)=(.*?)" alt="PHP Logo" /></a>'
-      .'<h1>PHP Version (.*?)</h1>(?:\n+?)</td></tr>#',
-      '#<h1><a href="(?:.*?)\?=(.*?)">PHP Credits</a></h1>#',
-      '#<tr>(?:.*?)" src="(?:.*?)=(.*?)"(?:.*?)Zend Engine (.*?),(?:.*?)</tr>#',
-      "# +#", '#<tr>#', '#</tr>#'),
-    array('$1', '', '', '', '</$1>' . "\n", '<', ' ', ' ', ' ', '', ' ',
-      '<h2>PHP Configuration</h2>'."\n".'<tr><td>PHP Version</td><td>$2</td></tr>'.
-      "\n".'<tr><td>PHP Egg</td><td>$1</td></tr>',
-      '<tr><td>PHP Credits Egg</td><td>$1</td></tr>',
-      '<tr><td>Zend Engine</td><td>$2</td></tr>' . "\n" .
-      '<tr><td>Zend Egg</td><td>$1</td></tr>', ' ', '%S%', '%E%'),
-    ob_get_clean());
-
-    $sections = explode('<h2>', strip_tags($pi, '<h2><th><td>'));
-    unset($sections[0]);
-
-    $pi = array();
-    foreach($sections as $section)
-    {
-       $n = substr($section, 0, strpos($section, '</h2>'));
-       preg_match_all('#%S%(?:<td>(.*?)</td>)?(?:<td>(.*?)</td>)?(?:<td>(.*?)</td>)?%E%#', $section, $askapache, PREG_SET_ORDER);
-       foreach($askapache as $m)
-       {
-         $pi[$n][$m[1]]=(!isset($m[3])||$m[2]==$m[3])?$m[2]:array_slice($m,2);
-       }
-    }
-
-    return $pi;
-  }
-
 }
 
 ?>
